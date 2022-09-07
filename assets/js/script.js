80cbc2d435686e3c053d6d5baa4d1dda23194807
IzzyKhalifa <izzy.e.khalifa@gmail.com>
2022-09-07 13:09:50 +1000

    Logic completed missing styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,15 @@ const content = document.querySelector(".container");
 const questionEl = document.querySelector(".question");
 const answersEl = document.querySelector(".info");
 const startPage = document.getElementById("p0");
+const p5 = document.getElementById("p5");
+const score = document.getElementById("score");
 var result = document.querySelector(".result");
+var playerName = document.getElementById("initials");
+const submitButton = document.getElementById("submitButton");
+const clearButton = document.getElementById("clearScore");
+const backButton = document.getElementById("goBack");
+var nameList = document.getElementById("names");
+const finalScore = 0;
 
 var rButtons = document.querySelectorAll(".right");
 for (const button of rButtons) {
@@ -22,24 +30,71 @@ for (const button of wButtons) {
 const start = document.querySelector(".startButton");
 start.addEventListener("click", execute);
 
-function nextPage() {
-  if (questNumber < 4) {
-    document.getElementById("p" + questNumber).style.display = "none";
-    questNumber++;
-    document.getElementById("p" + questNumber).style.display = "block";
-  } else {
-    questNumber = 5;
-  }
+function goBack() {
+  playerName.value = "";
+  timeLeft = 75;
+  nextPage();
+}
+backButton.addEventListener("click", goBack);
+
+function clearScore() {
+  names.innerHTML = "";
+}
+clearButton.addEventListener("click", clearScore);
+
+const maxScores = 5;
+
+function setHighScores(highScores) {
+  nameList.innerHTML = "";
+
+  highScores.forEach((score) => {
+    li = document.createElement("li");
+    li.innerHTML = score.timeLeft + " " + score.playerInitials;
+    nameList.appendChild(li);
+  });
 }
 
+function sortingHighscores() {
+  const highScores = localStorage.highScores
+    ? JSON.parse(localStorage.highScores)
+    : [];
+  console.log(highScores);
+  highScores.push({
+    timeLeft: timeLeft,
+    playerInitials: playerName.value,
+  });
+  highScores.sort((a, b) => b.timeLeft - a.timeLeft);
+  highScores.splice(5);
+  localStorage.removeItem("highScores");
+  localStorage.setItem("highScores", JSON.stringify(highScores));
+  setHighScores(highScores);
+  console.log(highScores);
+}
+
+function nextPage() {
+  if (questNumber > 3) {
+    names = document.getElementById("names");
+    result.style.display = "none";
+  }
+
+  if (questNumber == 5) {
+    sortingHighscores();
+  }
+
+  document.getElementById("p" + questNumber).style.display = "none";
+  questNumber++;
+  questNumber = questNumber % 7;
+  document.getElementById("p" + questNumber).style.display = "block";
+}
+
+submitButton.addEventListener("click", nextPage);
+
 function correctAnswer() {
-  console.log("Correct triggered");
   result.textContent = "Correct!!";
   result.style.display = "block";
   nextPage();
 }
 function wrongAnswer() {
-  console.log("Wrong triggered");
   result.textContent = "Wrong!!";
   result.style.display = "block";
   timeLeft -= 10;
@@ -49,6 +104,7 @@ function wrongAnswer() {
 function startTest() {
   startPage.style.display = "none";
   document.getElementById("p" + questNumber).style.display = "block";
+  nextPage();
 }
 
 function startCountdown() {
@@ -56,9 +112,9 @@ function startCountdown() {
     timeLeft--;
     timeEl.textContent = "Time: " + timeLeft;
 
-    if (timeLeft < 0) {
+    if (timeLeft < 0 || questNumber == 5) {
       clearInterval(countDown);
-      timeEl.textContent = "Game Over !!";
+      score.textContent = "Your final score is " + timeLeft;
     }
   }, 1000);
 }
